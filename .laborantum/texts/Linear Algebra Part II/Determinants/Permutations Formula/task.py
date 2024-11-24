390c575a19945b6d246359e2a978bb9b72f58dc6
storm <stormurbjorn24@gmail.com>
2024-11-24 13:48:39 +0700

    init commit

--- .laborantum/texts/Linear Algebra Part II/Determinants/Permutations Formula/task.py
+++ .laborantum/texts/Linear Algebra Part II/Determinants/Permutations Formula/task.py
@@ -4,33 +4,67 @@ from pathlib import Path
 
 home_dir = Path(__file__).parent.resolve()
 
+matrix_cycle = [1, 2, 3, 4]
+
+
+result = []
+
+n = len(matrix_cycle)
+
+for i in range(n):
+
+    matrix_cycle = matrix_cycle[1:] + [matrix_cycle[0]]
+    # result.append(matrix_cycle)
+    
+    result.append(" ".join(map(str, matrix_cycle)))
+
+print(result)
+
+reversed_matrix_cycle = matrix_cycle[::-1]
+n = len(reversed_matrix_cycle)
+
+for i in range(n):
+    
+    reversed_matrix_cycle = reversed_matrix_cycle[1:] + [reversed_matrix_cycle[0]]
+    # result.append(reversed_matrix_cycle)
+    result.append(" ".join(map(str, reversed_matrix_cycle)))
+
+print(result)
+
+
 answer = {
         "2x2": {
             "+": sorted([
-### YOUR CODE HERE ###
+                "1 2",
+                "2 1"
             ]),
             "-": sorted([
-### YOUR CODE HERE ###
             ])
         },
         "3x3": {
             "+": sorted([
-### YOUR CODE HERE ###
+                "1 2 3",
+                "2 3 1",
+                "3 1 2"
+                
             ]),
             "-": sorted([
-### YOUR CODE HERE ###
+                "2 1 3",
+                "1 3 2",
+                "3 2 1"
             ])
         },
         "4x4": {
             "+": sorted([
-### YOUR CODE HERE ###
+                "2 3 4 1", "3 4 1 2", "4 1 2 3", "1 2 3 4"
+                
             ]),
             "-": sorted([
-### YOUR CODE HERE ###
+                "2 3 4 1", "3 4 1 2", "4 1 2 3", "1 2 3 4", "3 2 1 4", "2 1 4 3", "1 4 3 2", "4 3 2 1"
             ])
         }
     }
 
 json.dump(
     answer, 
-    open(home_dir/Path('.answer.json'), "w+"))
+    open(home_dir/Path(".answer.json"), "w+"))
